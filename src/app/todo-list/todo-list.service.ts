cd66eb5b54dbefbe81951f23e3e11cce7d9b8231
loulou <loulou@sun.net.tw>
2021-02-18 18:20:19 +0800

    (練3-3) Step 3 . 將 S 引入資料模型，取代 string[]
    修改 srevice : todo-list.service.ts 的寫法，String[]  改為 todo資料模型
     * 匯入 todo
        * ./todo.model
     *使用  Todo 物件 代替 list 的 String 屬性
     *list.push 使用 todo

--- src/app/todo-list/todo-list.service.ts
+++ src/app/todo-list/todo-list.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Todo } from './todo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoListService {
-  private list : string[] = [];
+  private list : Todo[] = [];
 
   constructor() { }
   
   add(title:string):void{
     if(title || title.trim() )
-      this.list.push(title);
+      this.list.push(new Todo(title));
   }
 
-  getList(){
+  getList() : Todo[]{
     return this.list;
   }
 }
