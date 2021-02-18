2018d467ce0d4f8d5e913a7769efc306e437ac65
loulou <loulou@sun.net.tw>
2021-02-18 08:19:00 +0800

    (練3-3)  Step 2. 於 todo model 撰寫 getter , 與 切換 toggle
    2-1.撰寫 getter，兩種寫法 : 與 get 分開 或 連在一起
      * get done() 與 getTitle()
    
    2-2.撰寫 toggleCompletion() 來回切換
      * 是否完成 complated 是 checkbox
      * 對應於 checkbox 是打勾/取消打勾，可使用  boolean = !boolean 切換 true/false

--- src/app/todo-list/todo.model.ts
+++ src/app/todo-list/todo.model.ts
@@ -6,4 +6,19 @@ export class Todo {
         // 使用 建構子 新增 todo
         this.title = title;
     }
+
+    getTitle(): string{
+        // 取得 todo 名稱
+        return this.title;
+    }
+
+    get done() : boolean{
+        // 取得該 todo 是否完成
+        return this.completed;
+    }
+
+    toggleCompletion() : void {
+        // 切換 是否完成此 todo
+        this.completed = ! this.completed;
+    }
 }
